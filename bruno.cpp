25a81140b53858a293a8eb1bb1865a7e50bc30bd
Isaac <jannealvillamin@gmail.com>
2022-05-08 23:44:31 +0800

    Adding bruno.cpp

---
+++ bruno.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if (x == 0 && y == 0) {
+        cout << "BruNoNoNo";
+    }
+    else {
+        cout << "BruYesYesYes";
+    }
+}
