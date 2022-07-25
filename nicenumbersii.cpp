8f873ccd5dc0488b904b8066cf2b0d86727bf083
Isaac <jannealvillamin@gmail.com>
2022-07-26 01:24:11 +0800

    Revisit

--- nicenumbersii.cpp
+++ nicenumbersii.cpp
@@ -4,29 +4,29 @@ using namespace std;
 const long long N = 1e18;
 
 int lower_bound(vector<long long> a, long long x) {
-    int l = 0, r = a.size();
-    while (l < r) {
-        int m = l + (r - l) / 2;
+    int d = 0, r = a.size();
+    while (d < r) {
+        int m = d + (r - d) / 2;
         if (x <= a[m]) {
             r = m;
         } else {
-            l = m + 1;
+            d = m + 1;
         }
     }
-    return l;
+    return d;
 }
 
 int upper_bound(vector<long long> a, long long x) {
-    int l = 0, r = a.size();
-    while (l < r) {
-        int m = l + (r - l) / 2;
+    int d = 0, r = a.size();
+    while (d < r) {
+        int m = d + (r - d) / 2;
         if (x >= a[m]) {
-            l = m + 1;
+            d = m + 1;
         } else {
             r = m;
         }
     }
-    return l;
+    return d;
 }
 
 int main() {
@@ -34,25 +34,29 @@ int main() {
     cin.tie(0);
     vector<long long> a; // precalc nice numbers
     long long p2 = 1;
-    for (int i = 0; i <= 59 && p2 <= N; i++, p2 *= 2) {
+    while (p2 <= N) {
         long long p3 = 1;
-        for (int j = 0; j <= 37 && p2 * p3 <= N; j++, p3 *= 3) {
+        while (p2 * p3 <= N) {
             long long p5 = 1;
-            for (int k = 0; k <= 25 && p2 * p3 * p5 <= N; k++, p5 *= 5) {
+            while (p2 * p3 * p5 <= N) {
                 long long p7 = 1;
-                for (int l = 0; l <= 21 && p2 * p3 * p5 * p7 <= N; l++, p7 *= 7) {
+                while (p2 * p3 * p5 * p7 <= N) {
                     a.push_back(p2 * p3 * p5 * p7);
+                    p7 *= 7;
                 }
+                p5 *= 5;
             }
+            p3 *= 3;
         }
+        p2 *= 2;
     }
     sort(a.begin(), a.end()); // sort to do binary search
     int tt;
     cin >> tt;
     while (tt--) {
-        long long l, r;
-        cin >> l >> r;
-        int lb = lower_bound(a, l);
+        long long d, r;
+        cin >> d >> r;
+        int lb = lower_bound(a, d);
         int rb = upper_bound(a, r);
         cout << rb - lb << '\n';
     }
