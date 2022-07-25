8d5959366be3dd4f7ea2e04601901a459b6f4bba
Isaac <jannealvillamin@gmail.com>
2022-07-25 12:20:29 +0800

    Revisit

--- hashdle.cpp
+++ hashdle.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int n;
     cin >> n;
-    string s, t, ans;
-    cin >> s;
-    cin >> t;
-    map<char, int> mp;
-    for (int i = 0; i < n; i++) {
-        mp[s[i]] = 1;
+    string s, t;
+    cin >> s >> t;
+    map<char, bool> mp;
+    for (char c : s) {
+        mp[c] = true;
     }
+    string ans = "";
     for (int i = 0; i < n; i++) {
         if (s[i] == t[i]) {
             ans += 'G';
-        }
-        else if (mp[t[i]] == 1) {
+        } else if (mp[t[i]]) {
             ans += 'Y';
-        }
-        else {
+        } else {
             ans += '.';
         }
     }
-    cout << ans;
+    cout << ans << '\n';
+    return 0;
 }
