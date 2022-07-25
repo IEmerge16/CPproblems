ea90788999895377a027a2cf2ba3f0b1c0c14635
Isaac <jannealvillamin@gmail.com>
2022-07-25 14:55:01 +0800

    Revisit

--- nicenumbersii.cpp
+++ nicenumbersii.cpp
@@ -6,9 +6,7 @@ const long long max_N = 1e18;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    // precalculate nice numbers
-    set<long long> st;
+    set<long long> st; // precalc nice numbers
     long long p2 = 1;
     for (int i = 0; i <= 59 && p2 <= max_N; i++, p2 *= 2) {
         long long p3 = 1;
@@ -22,26 +20,25 @@ int main() {
             }
         }
     }
-
-    // index nice numbers
+    set<pair<long long, int>> st2; // index set of nice numbers
     int i = 1;
-    set<pair<long long, int>> st2;
     for (long long N : st) {
         st2.insert({N, i});
         i++;
     }
-
     int tt;
     cin >> tt;
     while (tt--) {
         long long l, r;
         cin >> l >> r;
-        auto lb = *st2.lower_bound({l, 0});
-        auto rb = *st2.upper_bound({r, st.size()});
-        int lc = lb.first, li = lb.second;
-        int rc = rb.first, ri = rb.second;
+        auto lb = st2.lower_bound({l, 0});
+        auto rb = st2.upper_bound({r, st.size()});
+        auto lp = *lb, rp = *rb;
+        int li = lp.second, ri = rp.second;
+        if (rb == st2.end()) {
+            ri = st.size() + 1;
+        }
         cout << ri - li << '\n';
     }
-
     return 0;
 }
