140fd777f272d6b0d5e80617e761c2d02e82d7d4
Isaac <jannealvillamin@gmail.com>
2022-07-26 01:27:09 +0800

    Revist

--- paruparog.cpp
+++ paruparog.cpp
@@ -14,7 +14,7 @@ int main() {
     cin.tie(0);
     int n;
     cin >> n;
-    for (int i = 1; i < 2 * n + 1; i++) {
+    for (int i = 1; i <= 2 * n; i++) {
         string ans = "";
         ans += '(';
         if (i < n) {
