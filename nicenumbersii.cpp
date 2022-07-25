63810e0e251f2b08b0f1db9ce517061c15475d26
Isaac <jannealvillamin@gmail.com>
2022-07-25 14:49:24 +0800

    Revisit

--- nicenumbersii.cpp
+++ nicenumbersii.cpp
@@ -1,79 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Finds where a target value (in this case, for L and R) should be in the array
-// Uses an iterative binary search algorithm
-int find_index(long long arr[], int n, long long x, string d) {
-    // Left and right pointers (bounds)
-    int left = 0;
-    int right = n - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        // If the target value is in the array, return the mid index
-        if (arr[mid] == x) {
-            return mid;
-        }
-        // If the target value is greater than the middle element, disregard the left half
-        else if (arr[mid] < x) {
-            left = mid + 1;
-        }
-        // If the target value is less than the middle element, disregard the right half
-        else {
-            right = mid - 1;
-        }
-    }
-    // Otherwise, if the index is for L, return right + 1, else, just right
-    if (d == "left") {
-        return right + 1;
-    }
-    else {
-        return right;
-    }
-}
+const long long max_N = 1e18;
 
 int main() {
-    int T;
-    cin >> T;
-    // Algorithm that generates nice numbers n <= 10^18 using balls and urns theorem
-    // Initiate a, b, c, d, the exponents of 2, 3, 5, 7, respectively, in the prime factorization of n
-    int in = 0, a, b, c, d;
-    long long arr[66061];
-    // For loop of the possible values of a + b + c + d (0 <= a + b + c + d <= 59)
-    for (int r = 0; r < 60; r++) {
-        // Three for loops that perform the balls and urns algorithm
-        // Visual representation when r = a + b + c + d = 4: 
-        // |||****, ||*|***, ..., ||****|, |*||***, |*|*|**, ..., |****||, ..., *|||***, ..., ****|||
-        for (int i = 0; i < r + 1; i++) { // The first '|'
-            for (int j = 0; j < r - i + 1; j++) { // The second '|'
-                for (int k = 0; k < r - i - j + 1; k++) { // The third '|'
-                    long long n;
-                    a = i;
-                    b = j;
-                    c = k;
-                    d = r - i - j - k;
-                    // Plug a, b, c, d 
-                    n = (pow(2, a)) * (pow(3, b)) * (pow(5, c)) * (pow(7, d));
-                    // Check whether the nice number n does not exceed 10^18
-                    // Must check n > 0 since it might exceed the maximum value of a long long data type
-                    if (n <= pow(10, 18) && n > 0) {
-                        // If it is within the range, add n to the array
-                        arr[in] = n;
-                        in++;
-                    }
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    // precalculate nice numbers
+    set<long long> st;
+    long long p2 = 1;
+    for (int i = 0; i <= 59 && p2 <= max_N; i++, p2 *= 2) {
+        long long p3 = 1;
+        for (int j = 0; j <= 37 && p2 * p3 <= max_N; j++, p3 *= 3) {
+            long long p5 = 1;
+            for (int k = 0; k <= 25 && p2 * p3 * p5 <= max_N; k++, p5 *= 5) {
+                long long p7 = 1;
+                for (int l = 0; l <= 21 && p2 * p3 * p5 * p7 <= max_N; l++, p7 *= 7) {
+                    st.insert(p2 * p3 * p5 * p7);
                 }
             }
         }
     }
-    // Sort the array to perform binary search and find where L and R are
-    sort(arr, arr + 66061);
-    while (T--) {
-        long long L, R;
-        cin >> L >> R;
-        // Represent the index where L and R, respectively, can be found in the array
-        int x, y;
-        x = find_index(arr, 66061, L, "left");
-        y = find_index(arr, 66061, R, "right");
-        // Print y - x + 1, the number of nice numbers between L and R
-        cout << y - x + 1 << endl;
+
+    // index nice numbers
+    int i = 1;
+    set<pair<long long, int>> st2;
+    for (long long N : st) {
+        st2.insert({N, i});
+        i++;
     }
+
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        long long l, r;
+        cin >> l >> r;
+        auto lb = *st2.lower_bound({l, 0});
+        auto rb = *st2.upper_bound({r, st.size()});
+        int lc = lb.first, li = lb.second;
+        int rc = rb.first, ri = rb.second;
+        cout << ri - li << '\n';
+    }
+
+    return 0;
 }
