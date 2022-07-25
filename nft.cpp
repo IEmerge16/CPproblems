f2000f2e89a22c93fcc2d0a5202e3a8c0afb5e82
Isaac <jannealvillamin@gmail.com>
2022-07-26 04:43:07 +0800

    Revisit

--- nft.cpp
+++ nft.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int n;
     cin >> n;
-    map<long long, bool> sub;
-    map<long long, long long> bot;
-    map<long long, long long> top;
-    long long pos = 0, tot = 0;
+    map<ll, bool> sub;
+    map<ll, ll> bot;
+    map<ll, ll> top;
+    ll pos = 0, tot = 0;
     for (int i = 0; i < n; i++) {
         string s;
         cin >> s;
@@ -22,22 +24,18 @@ int main() {
         } else {
             int y;
             cin >> y;
-            long long cur = y - tot;
+            ll cur = y - tot;
             sub[cur] = true;
             bot[cur] = cur;
             top[cur] = cur;
-            if (sub[cur - 1] && sub[cur + 1]) {
+            if (sub[cur - 1]) {
                 bot[cur] = bot[cur - 1];
-                top[cur] = top[cur + 1];
-                top[cur - 1] = top[cur];
-                bot[cur + 1] = bot[cur];
-            } else if (sub[cur - 1]) {
-                bot[cur] = bot[cur - 1];
-                top[cur - 1] = top[cur];
-            } else if (sub[cur + 1]) {
-                top[cur] = top[cur + 1];
-                bot[cur + 1] = bot[cur];
             }
+            if (sub[cur + 1]) {
+                top[cur] = top[cur + 1];
+            }
+            top[bot[cur]] = top[cur];
+            bot[top[cur]] = bot[cur];
             if (sub[pos]) {
                 cout << top[pos] - pos + 1 << '\n';
                 pos = top[pos] + 1;
