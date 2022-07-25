70877f92026a457abd57123d5332551e4ad8f9b8
Isaac <jannealvillamin@gmail.com>
2022-07-25 23:18:01 +0800

    Revisit

--- nicenumbersii.cpp
+++ nicenumbersii.cpp
@@ -3,10 +3,36 @@ using namespace std;
 
 const long long max_N = 1e18;
 
+int lower_bound(int n, vector<long long> a, long long x) {
+    int l = 0, r = n - 1;
+    while (l < r) {
+        int mid = l + (r - l) / 2;
+        if (x <= a[mid]) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return l;
+}
+
+int upper_bound(int n, vector<long long> a, long long x) {
+    int l = 0, r = n - 1;
+    while (l < r) {
+        int mid = l + (r - l) / 2;
+        if (x >= a[mid]) {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
+    }
+    return l;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    set<long long> st; // precalc nice numbers
+    vector<long long> a; // precalc nice numbers
     long long p2 = 1;
     for (int i = 0; i <= 59 && p2 <= max_N; i++, p2 *= 2) {
         long long p3 = 1;
@@ -15,30 +41,20 @@ int main() {
             for (int k = 0; k <= 25 && p2 * p3 * p5 <= max_N; k++, p5 *= 5) {
                 long long p7 = 1;
                 for (int l = 0; l <= 21 && p2 * p3 * p5 * p7 <= max_N; l++, p7 *= 7) {
-                    st.insert(p2 * p3 * p5 * p7);
+                    a.push_back(p2 * p3 * p5 * p7);
                 }
             }
         }
     }
-    set<pair<long long, int>> st2; // index set of nice numbers
-    int i = 1;
-    for (long long N : st) {
-        st2.insert({N, i});
-        i++;
-    }
+    sort(a.begin(), a.end());
     int tt;
     cin >> tt;
     while (tt--) {
         long long l, r;
         cin >> l >> r;
-        auto lb = st2.lower_bound({l, 0});
-        auto rb = st2.upper_bound({r, st.size()});
-        auto lp = *lb, rp = *rb;
-        int li = lp.second, ri = rp.second;
-        if (rb == st2.end()) {
-            ri = st.size() + 1;
-        }
-        cout << ri - li << '\n';
+        int lb = lower_bound(a.size(), a, l);
+        int rb = upper_bound(a.size(), a, r);
+        cout << rb - lb << '\n';
     }
     return 0;
 }
